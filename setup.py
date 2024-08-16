d692222ec543a7db98ea4b16924eb50b92ba8818
vladimir.gorea <vladimir.gorea@lionstep.com>
2024-08-16 12:03:39 +0300

    up version to 0.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="data_persistence_repository",
-    version="0.2.2",
+    version="0.2.3",
     author="vladimir gorea",
     author_email="vladimir@smileservices.dev",
     description="A repository implementation for data persistence.",
